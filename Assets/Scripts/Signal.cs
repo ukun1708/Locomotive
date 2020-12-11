4e32fd3011c3a5cdb6a3525ce555b56e6ed9e380
Виталий Лукин <mr.lukin1990@mail.ru>
2020-12-11 18:33:17 +0300

    Create signal

---
+++ Assets/Scripts/Signal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Signal : MonoBehaviour
+{
+    AudioSource audio;
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+
+        audio.enabled = false;
+    }
+    public void SignalOn()
+    {
+        StartCoroutine(Gudok());
+    }
+
+    IEnumerator Gudok()
+    {
+        audio.enabled = true;
+
+        yield return new WaitForSeconds(2);
+
+        audio.enabled = false;
+
+        yield return null;
+    }
+}
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,27 +7,27 @@ public class Timer : MonoBehaviour
 {
     Text clockText;
 
-    public float minut, second1, second2;
+    public float minut, second;
     
     void Start()
     {
         clockText = GetComponent<Text>();
 
         minut = 5f;
-        second2 = 60f;
+        second = 60f;
     }
 
     
     void Update()
     {
-        second2 -= 1f * Time.deltaTime;
+        second -= 1f * Time.deltaTime;
 
-        if (second2 < 10f)
+        if (second < 1f)
         {
             minut -= 1f;
-            second2 = 60f;
+            second = 60f;
         }
 
-        clockText.text = minut.ToString("F0") + ":" + second2.ToString("F0");
+        clockText.text = minut.ToString("F0") + ":" + second.ToString("F0");
     }
 }
